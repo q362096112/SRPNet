fd7b3fffdd7c3b5a63b3f3ea5929dfc38afaae73
huangyao <362096112@qq.com>
2023-07-18 19:23:42 +0800

    first commit

--- examples/test_ssl_abdominal_mri.sh
+++ examples/test_ssl_abdominal_mri.sh
@@ -46,7 +46,7 @@ do
         mkdir $LOGDIR
     fi
 
-    RELOAD_PATH='./exps/myexperiments_MIDDLE_0/mySSL_train_CHAOST2_Superpix_lbgroup0_scale_MIDDLE_vfold0_CHAOST2_Superpix_sets_1_1shot/1/snapshots/100000.pth' # path to the reloaded model
+    RELOAD_PATH='' # path to the reloaded model
 
     python3 validation.py with \
     'modelname=dlfcn_res101' \
